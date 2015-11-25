21c3ca6e051c3889f2fde67bc54b2b46781168fb
Etienne Jézéquel <etienne.jezequel@icloud.com>
2015-11-25 17:44:33 +0100

    Importante update : new "parcours", correct JS problems

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,91 +1,85 @@
 $(document).ready(function()
-{
-$("#requiem").hide();
-$(".logo-logic").hide();
-$(".logo-autre-c").hide();
+                  {
+    $("#requiem").hide();
+    $(".logo-logic").hide();
+    $(".logo-autre-c").hide();
 
-    $("#formulaire").formValidation({
- 		mail : "mail",
-                corps : "corps",
-		objet : "objet"
+
+    $( "body" ).click(function( event ) {
+        console.log(event.target.id);
+        if(event.target.id == "langage"){
+            if ($("#logiciel").hasClass("on")==true){
+                $("#logiciel").removeClass("on");
+                $(".logo-logic").hide();
+            }
+
+            if ($("#autre-c").hasClass("on")==true){
+                $("#autre-c").removeClass("on");
+                $(".logo-autre-c").hide();
+            }
+
+            if ($("#langage").hasClass("on")==false){
+                $("#langage").addClass("on");
+                $(".logo-lang").fadeIn();
+            }
+        }
+
+        if(event.target.id == "autre-c"){
+            if ($("#logiciel").hasClass("on")==true){
+                $("#logiciel").removeClass("on");
+                $(".logo-logic").hide();
+            }
+
+            if ($("#langage").hasClass("on")==true){
+                $("#langage").removeClass("on");
+                $(".logo-lang").hide();
+            }
+
+            if ($("#autre-c").hasClass("on")==false){
+                $("#autre-c").addClass("on");
+                $(".logo-autre-c").fadeIn();
+            }
+
+
+        }
+
+        if(event.target.id == "logiciel"){
+            if ($("#autre-c").hasClass("on")==true){
+                $("#autre-c").removeClass("on");
+                $(".logo-autre-c").hide();
+            }
+
+            if ($("#langage").hasClass("on")==true){
+                $("#langage").removeClass("on");
+                $(".logo-lang").hide();
+            }
+
+
+            if ($("#logiciel").hasClass("on")==false){
+                $("#logiciel").addClass("on");
+                $(".logo-logic").fadeIn();
+            }
+
+        }
     });
-
-
-$( "body" ).click(function( event ) {
-  console.log(event.target.id);
-  if(event.target.id == "langage"){
-	  if ($("#logiciel").hasClass("on")==true){
-		 $("#logiciel").removeClass("on");
-		  $(".logo-logic").hide();
-	  }
-
-	  if ($("#autre-c").hasClass("on")==true){
-		 $("#autre-c").removeClass("on");
-		 $(".logo-autre-c").hide();
-	  }
-
-		if ($("#langage").hasClass("on")==false){
-	   $("#langage").addClass("on");
-	   $(".logo-lang").fadeIn();
-		}
-  }
-
-  if(event.target.id == "autre-c"){
-	   if ($("#logiciel").hasClass("on")==true){
-		 $("#logiciel").removeClass("on");
-		 $(".logo-logic").hide();
-	  }
-
-	  if ($("#langage").hasClass("on")==true){
-		 $("#langage").removeClass("on");
-		 $(".logo-lang").hide();
-	  }
-
-		if ($("#autre-c").hasClass("on")==false){
-	   $("#autre-c").addClass("on");
-	   $(".logo-autre-c").fadeIn();
-		}
-
-
-  }
-
-  if(event.target.id == "logiciel"){
-	  if ($("#autre-c").hasClass("on")==true){
-		 $("#autre-c").removeClass("on");
-		 $(".logo-autre-c").hide();
-	  }
-
-	  if ($("#langage").hasClass("on")==true){
-		 $("#langage").removeClass("on");
-		 $(".logo-lang").hide();
-	  }
-
-
-		if ($("#logiciel").hasClass("on")==false){
-	   $("#logiciel").addClass("on");
-	   $(".logo-logic").fadeIn();
-		}
-
-  }
- });
 
 });
 
 
 (function($){
 
-	launch = function() {
-		if($(".requiem").attr("id") == "requiem-active"){
-			$(".requiem").slideUp();
-			$(".requiem").attr("id","requiem");
-		}
+    launch = function() {
+        if($(".requiem").attr("id") == "requiem-active"){
+            $(".requiem").slideUp();
+            $(".requiem").attr("id","requiem");
+        }
 
-		else {
-		 $(".requiem").slideDown();
-		 $(".requiem").attr("id","requiem-active");
-		 }
+        else {
+            $(".requiem").slideDown();
+            $(".requiem").attr("id","requiem-active");
+        }
 
-	};
+    };
 
 
 
@@ -94,8 +88,8 @@ $( "body" ).click(function( event ) {
 
 
 function menu (choix){
-	if(choix == "langages"){
+    if(choix == "langages"){
 
-	}
+    }
 
 }
