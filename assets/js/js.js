10bad4a584d4d6df7b49b24385fcfa6cc2dd238c
Etienne Jézéquel <etienne.jezequel@icloud.com>
2015-12-04 11:15:36 +0100

    Update : reorganise index.html, clean js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,95 +1,13 @@
-$(document).ready(function()
-                  {
-    $("#requiem").hide();
-    $(".logo-logic").hide();
-    $(".logo-autre-c").hide();
-
+$(document).ready(function(){
 
     $( "body" ).click(function( event ) {
         console.log(event.target.id);
-        if(event.target.id == "langage"){
-            if ($("#logiciel").hasClass("on")==true){
-                $("#logiciel").removeClass("on");
-                $(".logo-logic").hide();
-            }
-
-            if ($("#autre-c").hasClass("on")==true){
-                $("#autre-c").removeClass("on");
-                $(".logo-autre-c").hide();
-            }
-
-            if ($("#langage").hasClass("on")==false){
-                $("#langage").addClass("on");
-                $(".logo-lang").fadeIn();
-            }
+        if(event.target.id == "telecombretagne"){
+            alert("lol");
         }
 
-        if(event.target.id == "autre-c"){
-            if ($("#logiciel").hasClass("on")==true){
-                $("#logiciel").removeClass("on");
-                $(".logo-logic").hide();
-            }
-
-            if ($("#langage").hasClass("on")==true){
-                $("#langage").removeClass("on");
-                $(".logo-lang").hide();
-            }
-
-            if ($("#autre-c").hasClass("on")==false){
-                $("#autre-c").addClass("on");
-                $(".logo-autre-c").fadeIn();
-            }
-
-
-        }
-
-        if(event.target.id == "logiciel"){
-            if ($("#autre-c").hasClass("on")==true){
-                $("#autre-c").removeClass("on");
-                $(".logo-autre-c").hide();
-            }
-
-            if ($("#langage").hasClass("on")==true){
-                $("#langage").removeClass("on");
-                $(".logo-lang").hide();
-            }
-
-
-            if ($("#logiciel").hasClass("on")==false){
-                $("#logiciel").addClass("on");
-                $(".logo-logic").fadeIn();
-            }
-
-        }
     });
 
 });
 
 
-(function($){
-
-    launch = function() {
-        if($(".requiem").attr("id") == "requiem-active"){
-            $(".requiem").slideUp();
-            $(".requiem").attr("id","requiem");
-        }
-
-        else {
-            $(".requiem").slideDown();
-            $(".requiem").attr("id","requiem-active");
-        }
-
-    };
-
-
-
-
-})(jQuery)
-
-
-function menu (choix){
-    if(choix == "langages"){
-
-    }
-
-}
